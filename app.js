3ed1f03c0d8d28442b0e98a67b7b3270f8007908
Haley Little <hpl15@my.fsu.edu>
2018-11-25 14:00:48 -0600

    Master List updates

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//REQUIRED
 var express = require('express');
 var app = express();
 var path = require('path')
@@ -14,31 +15,28 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+
+//ROUTES
 //sign up page
 app.get('/signup', function(req, res) {
 	res.render('signup');
 });
-
 //master brewery page
 app.get('/master', function(req, res) {
 	res.render('master');
 });
-
 //proof page
 app.get('/proof', function(req, res) {
 	res.render('proof');
 });
-
 //grasslands page
 app.get('/grasslands', function(req, res) {
 	res.render('grasslands');
 });
-
 //deep page
 app.get('/deep', function(req, res) {
 	res.render('deep');
 });
-
 //ology page
 app.get('/ology', function(req, res) {
 	res.render('ology');
